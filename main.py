95a460e41eafacc0cc3090ced46bfdb93e5eeed0
1devorg <1DevPrime@gmail.com>
2023-10-15 21:57:47 +0500

    second commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ response = g4f.ChatCompletion.create(
               {"role": "assistant", "content": "Good morrow, fair sir or lady! Thou hast asked a most wondrous question, forsooth. Pray tell, what dost thou mean by 'context of conversation'? Dost thou mean the flow of our discourse or the subject matter we discuss?"},
               {"role": "user", "content": "Yes, I mean the course of our conversation"},
               {"role": "assistant", "content": "Ah, I understand now! To preserve the course of our conversation, fear not, for I, thy trusty AI assistant, shall keepeth track of all that hath been said. Whether thou dost asketh a question or maketh a statement, I shall remember and respond accordingly. Shouldst thou wish to revisit a previous topic or continue our discussion from where we left off, simply remind me of the matter at hand, and I shall gladly oblige. Verily, I am here to assist thee and ensure a seamless and coherent conversation."},
-              {"role": "user", "content": "напиши какое нибудь предложение на башкирском языке?"},
+              {"role": "user", "content": "напиши какое нибудь предложение на башкирском языке ?"},
               ],
 )
 
